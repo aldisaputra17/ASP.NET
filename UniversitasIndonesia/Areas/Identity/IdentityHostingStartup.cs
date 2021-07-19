8a8aceb7ee493434c7ab2398773850a0b622371d
aldisaputra17 <aldisaput17@gmail.com>
2021-07-19 11:44:21 +0700

    UniversitasIndonesia Auth login logout and crud mvc core 3.1

---
+++ UniversitasIndonesia/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using UniversitasIndonesia.Areas.Identity.Data;
+using UniversitasIndonesia.Data;
+
+[assembly: HostingStartup(typeof(UniversitasIndonesia.Areas.Identity.IdentityHostingStartup))]
+namespace UniversitasIndonesia.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<UniversitasIndonesiaDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("UniversitasIndonesiaDbContextConnection")));
+
+                services.AddDefaultIdentity<UniversitasIndonesiaUser>(options =>
+                {
+                    options.SignIn.RequireConfirmedAccount = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+
+                })
+                     .AddEntityFrameworkStores<UniversitasIndonesiaDbContext>();
+
+            });
+            
+            
+        }
+    }
+}
---
+++ UniversitasIndonesia/Controllers/MahasiswaController.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UniversitasIndonesia.Data;
+using UniversitasIndonesia.Models;
+
+namespace UniversitasIndonesia.Controllers
+{
+    [Authorize]
+    public class MahasiswaController : Controller
+    {
+        private readonly UniversitasIndonesiaDbContext _db;
+
+
+        public MahasiswaController(UniversitasIndonesiaDbContext db)
+        {
+            _db = db;
+        }
+        //GET INDEX
+        public IActionResult Index()
+        {
+            IEnumerable<Mahasiswa> objList = _db.Mahasiswa;
+            return View(objList);
+        }
+        //GET CREATE
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST CREATE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Mahasiswa obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Mahasiswa.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+
+            return View(obj);
+
+
+        }
+
+        //GET EDIT
+        public IActionResult Edit(int id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Mahasiswa.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        //POST EDIT
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Mahasiswa obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Mahasiswa.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+
+            return View(obj);
+
+
+        }
+
+        //GET DELETE
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Mahasiswa.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        //POST DELETE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Mahasiswa.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _db.Mahasiswa.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+
+
+
+        }
+    }
+}
